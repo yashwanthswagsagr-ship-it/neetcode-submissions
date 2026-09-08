158c7ae5dc2c619763f74b674915246a55eeb6cd
Yashwanth <yashwanthswagsagr@gmail.com>
2026-09-08 19:24:51 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            // Skip spaces and special characters
+            while (left < right && !isalnum(s[left])) {
+                left++;
+            }
+
+            while (left < right && !isalnum(s[right])) {
+                right--;
+            }
+
+            if (tolower(s[left]) != tolower(s[right])) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
